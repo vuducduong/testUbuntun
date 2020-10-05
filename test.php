711319a04c51a63c088490a33884390a3d4d7a4b
vuducduong <vuducduong193@gmail.com>
2020-10-05 17:27:29 +0700

    add file

---
+++ test.php
@@ -0,0 +1,12 @@
+<?php
+$cars = array('a'=>"lamborgini",'b'=>"lexus","maybach");
+//for($x = 0;$x<count($cars);$x++){
+//    echo $cars[$x];
+//    echo "<br>";
+//}
+foreach ($cars as $item){
+    echo $item;
+    echo "<br>";
+
+}
+
